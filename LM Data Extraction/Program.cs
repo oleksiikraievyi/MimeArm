51cca3c124a8016290a4463b28dd29f6d9e2c679
Artem Cheberyak <flameboy.9k@gmail.com>
2015-10-05 13:24:51 +0300

    Created MSVS solution

---
+++ LM Data Extraction/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LM_Data_Extraction {
+	class Program {
+		static void Main(string[] args) {
+
+		}
+	}
+}
