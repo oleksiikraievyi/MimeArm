4d9b4e3c70678929825666d8054c94484a041c3e
Alex Kraevyy <clockworkalexorange@gmail.com>
2015-12-21 17:53:07 +0200

    Cleaned trash from repository and created new projects - project itself and unit testing project

---
+++ MimeArm.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MimeArm.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- MimeArm/Program.cs
+++ MimeArm/Program.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LM_Data_Extraction {
-	class Program {
-		static void Main(string[] args) {
-
-		}
-	}
+namespace MimeArm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
 }
