ca83043f186a41c6c74e2cde70c8d585f7ed2f1f
DanilaMulyavko1 <lkroksl@gmail.com>
2015-12-28 17:06:29 +0200

    Added Unity project to solution

---
+++ MimeArm/BusinessLayer/UnityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MimeArm.BusinessLayer;
+
+namespace MimeArm.BusinessLayer
+{
+    class UnityController : Controller<object>
+    {
+        protected override object TransferToView()
+        {
+            return new object();
+        }
+    }
+}
---
+++ MimeArm/Interfaces/UnityInterface.cs
@@ -0,0 +1,17 @@
+﻿using MimeArm.BusinessLayer;
+using System;
+using System.Diagnostics;
+
+namespace MimeArm.Interfaces
+{
+    class UnityInterface : Interface<object>
+    {
+        public UnityInterface(Controller<object> controller) : base(controller) { }
+
+        public override void SendDataToInterface(object sender, object t)
+        {
+            var process = new ProcessStartInfo(@"C:\Users\Danya\Documents\Visual Studio 2015\Projects\WindowsFormsApplication1\WindowsFormsApplication1\bin\Debug\WindowsFormsApplication.exe");
+            Process.Start(process);
+        }
+    }
+}
