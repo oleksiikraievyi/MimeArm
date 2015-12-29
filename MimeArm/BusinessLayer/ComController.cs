463904d0b837b3fad9a147c50fc09740f61e7b35
Artem Cheberyak <flameboy.9k@gmail.com>
2015-12-30 01:39:27 +0200

    Added future test snippets

--- MimeArm/BusinessLayer/ComController.cs
+++ MimeArm/BusinessLayer/ComController.cs
@@ -129,46 +129,57 @@ namespace MimeArm.BusinessLayer
         public static byte[] PrepareByteArrayToSend(byte header, float x, float y, float z, short wristAngle, short wristRotation, short gripper, byte speed, byte external)
         {
             List<byte> returnValue = new List<byte>();
-            short xShort = Convert.ToInt16(Math.Round(x));
-            short yShort = Convert.ToInt16(Math.Round(y));
-            short zShort = Convert.ToInt16(Math.Round(z));
-            byte xHigh = BitConverter.GetBytes(xShort)[1];
-            byte xLow = BitConverter.GetBytes(xShort)[0];
-            byte yHigh = BitConverter.GetBytes(zShort)[1];              // arm bug, consider that y and z are swapped!
-            byte yLow = BitConverter.GetBytes(zShort)[0];
-            byte zHigh = BitConverter.GetBytes(yShort)[1];
-            byte zLow = BitConverter.GetBytes(yShort)[0];
-            byte wristAngleHigh = BitConverter.GetBytes(wristAngle)[1];
-            byte wristAngleLow = BitConverter.GetBytes(wristAngle)[0];
-            byte wristRotationHigh = BitConverter.GetBytes(wristRotation)[1];
-            byte wristRotationLow = BitConverter.GetBytes(wristRotation)[0];
-            byte gripperAngleHigh = BitConverter.GetBytes(gripper)[1];
-            byte gripperAngleLow = BitConverter.GetBytes(gripper)[0];
-            byte DTIME = speed;                                         // set arm movement time 
-            byte BUTTONS = 0x00;                                        //should be 0 as we don't have any buttons
-            byte EXT = external;                                        // additional commands
-            byte checksum = Convert.ToByte(0xFF - (xHigh + xLow + yHigh + yLow + zHigh + zLow + wristAngleHigh +
-                wristAngleLow + wristRotationHigh + wristRotationLow + gripperAngleHigh + gripperAngleLow +
-                DTIME + BUTTONS + EXT) % 256);
 
-            //create COM package
-            returnValue.Add(header);
-            returnValue.Add(xHigh);
-            returnValue.Add(xLow);
-            returnValue.Add(yHigh);
-            returnValue.Add(yLow);
-            returnValue.Add(zHigh);
-            returnValue.Add(zLow);
-            returnValue.Add(wristAngleHigh);
-            returnValue.Add(wristAngleLow);
-            returnValue.Add(wristRotationHigh);
-            returnValue.Add(wristRotationLow);
-            returnValue.Add(gripperAngleHigh);
-            returnValue.Add(gripperAngleLow);
-            returnValue.Add(DTIME);
-            returnValue.Add(BUTTONS);
-            returnValue.Add(EXT);
-            returnValue.Add(checksum);
+            if (header == 0xFF)
+            {
+                short xShort = Convert.ToInt16(Math.Round(x));
+                short yShort = Convert.ToInt16(Math.Round(y));
+                short zShort = Convert.ToInt16(Math.Round(z));
+                byte xHigh = BitConverter.GetBytes(xShort)[1];
+                byte xLow = BitConverter.GetBytes(xShort)[0];
+                byte yHigh = BitConverter.GetBytes(zShort)[1];              // arm bug, consider that y and z are swapped!
+                byte yLow = BitConverter.GetBytes(zShort)[0];
+                byte zHigh = BitConverter.GetBytes(yShort)[1];
+                byte zLow = BitConverter.GetBytes(yShort)[0];
+                byte wristAngleHigh = BitConverter.GetBytes(wristAngle)[1];
+                byte wristAngleLow = BitConverter.GetBytes(wristAngle)[0];
+                byte wristRotationHigh = BitConverter.GetBytes(wristRotation)[1];
+                byte wristRotationLow = BitConverter.GetBytes(wristRotation)[0];
+                byte gripperAngleHigh = BitConverter.GetBytes(gripper)[1];
+                byte gripperAngleLow = BitConverter.GetBytes(gripper)[0];
+                byte DTIME = speed;                                         // set arm movement time 
+                byte BUTTONS = 0x00;                                        //should be 0 as we don't have any buttons
+                byte EXT = external;                                        // additional commands
+                byte checksum = Convert.ToByte(0xFF - (xHigh + xLow + yHigh + yLow + zHigh + zLow + wristAngleHigh +
+                    wristAngleLow + wristRotationHigh + wristRotationLow + gripperAngleHigh + gripperAngleLow +
+                    DTIME + BUTTONS + EXT) % 256);
+
+                //create COM package
+                returnValue.Add(header);
+                returnValue.Add(xHigh);
+                returnValue.Add(xLow);
+                returnValue.Add(yHigh);
+                returnValue.Add(yLow);
+                returnValue.Add(zHigh);
+                returnValue.Add(zLow);
+                returnValue.Add(wristAngleHigh);
+                returnValue.Add(wristAngleLow);
+                returnValue.Add(wristRotationHigh);
+                returnValue.Add(wristRotationLow);
+                returnValue.Add(gripperAngleHigh);
+                returnValue.Add(gripperAngleLow);
+                returnValue.Add(DTIME);
+                returnValue.Add(BUTTONS);
+                returnValue.Add(EXT);
+                returnValue.Add(checksum);
+            } 
+            else
+            {
+                byte checksum = Convert.ToByte(0xFF - external % 256);
+                returnValue.AddRange(new byte[15]);
+                returnValue.Add(external);
+                returnValue.Add(checksum);
+            }
 
             return returnValue.ToArray();
         }
@@ -218,6 +229,44 @@ namespace MimeArm.BusinessLayer
             Port.Close();
             Port.Dispose();
             base.Dispose();
+        }
+
+        public void testTrue_prepareByteArrayToSend()
+        {
+            var byteArray = PrepareByteArrayToSend(0, 100, 100, 100, 50, 100, 100, 30, 11);
+            //assert equals byte[] {255, 0, 100, 0, 100, 0, 100, 0, 50, 0, 100, 0, 100, 30, 0, 11, 176}
+        }
+
+        public void testFalse_prepareByteArrayToSend()
+        {
+            var byteArray = PrepareByteArrayToSend(0, 100, 100, 100, 50, 100, 100, 30, 11);
+            //assert equals byte[] {255, 6, 4, 0, 100, 0, 100, 0, 50, 0, 100, 0, 100, 30, 0, 11, 176}
+        }
+
+        public void testTrue_prepareByteArrayToSendWithEmptyHeader()
+        {
+            var byteArray = PrepareByteArrayToSend(255, 100, 100, 100, 50, 100, 100, 30, 11);
+            //assert equals byte[] {255, 0, 100, 0, 100, 0, 100, 0, 50, 0, 100, 0, 100, 30, 0, 11, 176}
+        }
+
+        public void testFalse_prepareByteArrayToSendWithEmptyHeader()
+        {
+            var byteArray = PrepareByteArrayToSend(0, 100, 100, 100, 50, 100, 100, 30, 11);
+            //assert equals byte[] {0, 6, 4, 0, 100, 0, 100, 0, 50, 0, 100, 0, 100, 30, 0, 11, 176}
+        }
+
+        public void testTrue_isPackageOk()
+        {
+            byte[] testValue = { 255, 0, 100, 0, 100, 0, 100, 0, 50, 0, 100, 0, 100, 30, 0, 11, 176 };
+            bool isOk = IsPackageOk(testValue);
+            //assert equals true
+        }
+
+        public void testFalse_isPackageOk()
+        {
+            byte[] testValue = { 255, 0, 100, 0, 100, 0, 100, 1, 50, 0, 100, 0, 100, 30, 0, 11, 176 };
+            bool isOk = IsPackageOk(testValue);
+            //assert equals false
         }
     }
 }
